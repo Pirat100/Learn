8cbedaf79b90d0ab387904b651f30c41dbc65891
Pirat100 <88029044+Pirat100@users.noreply.github.com>
2022-05-27 22:00:39 +0300

    Add files via upload

---
+++ LaforetObjectiveC++/Chapter8.OverloadOperations/Exercise2.cxx
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class String {
+private:
+    enum { SZ = 80 };
+    char str[SZ];
+public:
+    String() { strcpy(str, ""); }
+    String(char s[]) {
+        strcpy(str, s);	
+    }
+    void display() const {
+        cout << str;	
+    }
+    String operator+=(String ss) const {
+    	String temp;
+    	if (strlen(str) + strlen(ss.str) < SZ) {
+    		strcpy(temp.str, str);
+    		strcat(temp.str, ss.str);
+    	} else {
+    		cout << "\nПереполнение!";
+    		exit(1);
+    	}
+    	return temp;
+    }
+};
+
+int main()
+{
+    String s1 = "\nС Рождеством! ";
+    String s2 = "С Новым годом!";
+    String s3;
+    
+    s1.display();
+    s2.display();
+    s3.display();
+    
+    s3 = s1 + s2;
+    
+    s3.display();
+    cout << endl;
+	return 0;
+}
